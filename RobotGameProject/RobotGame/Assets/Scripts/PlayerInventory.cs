656e774497f01e721537088e81c0fe15d73d5177
lewlol <lewdevbusiness@gmail.com>
2023-04-08 21:54:32 +0100

    inventory

---
+++ RobotGameProject/RobotGame/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [Header("Items")]
+    public int rawMetal;
+    public int refinedMetal;
+    public int copper;
+
+    [Header("Texts")]
+    public GameObject Inventory;
+    public Text rawMetalText;
+    public Text refinedMetalText;
+    public Text copperText;
+
+    bool invActive;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("OpenInventory"))
+            OpenCloseInventory();
+    }
+    public void OpenCloseInventory()
+    {
+        invActive = !invActive;
+        Inventory.SetActive(invActive);
+
+        SetTexts();
+    }
+
+    public void SetTexts()
+    {
+        rawMetalText.text = rawMetal.ToString();
+        refinedMetalText.text = refinedMetal.ToString();
+        copperText.text = copper.ToString();
+    }
+}
